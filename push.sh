c4e6b2f4d4c65811afb7c89f772edd31ddcb1179
Yakov Beder <yakovbeder@gmail.com>
2023-06-26 17:38:21 +0300

    Prometheus and Grafana

--- push.sh
+++ push.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-# Prompt user for commit message
 echo "Enter commit message:"
 read commit_message
 
-# Add all files to the staging area
 git add .
 
-# Commit changes with the provided message
 git commit -m "$commit_message"
 
-# Push changes to the remote repository
 git push
